cf7c6cbb831b844030e511f2044dba1c52d6f06a
moses21 <moses21@student.ubc.ca>
2024-05-23 14:57:55 -0700

    began basic code for manager, created ItemType enum

---
+++ src/model/ItemType.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Represents the possible types of items a project can have.
+ */
+public enum ItemType {
+    BREEDSCROLL,
+    SPECIALTYSCROLL,
+    PRIMARYGENE,
+    SECONDARYGENE,
+    TERTIARYGENE,
+    EYEVIAL,
+    SKINCENT,
+    APPAREL,
+    SCENE,
+    FAMILIAR
+}
--- src/model/managers/Manager.java
+++ src/model/managers/Manager.java
@@ -1,7 +1,20 @@
 package model.managers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// TODO figure out how relate manager to its kids
 /**
  * Represents a module which can organize and sort various things.
  * */
 public abstract class Manager {
+    private ArrayList<Object> listToManage;
+    private HashMap<Object, Object> groupByTraits;
+
+    public void add() {}
+    public void delete() {}
+    public void rename() {}
+    public void edit() {}
+    public void filterBy() {}
+    public void markDone() {}
 }
