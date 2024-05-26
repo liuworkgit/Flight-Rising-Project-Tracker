ae552a607d80af1ae845d459545fe4e72acc314a
moses21 <moses21@student.ubc.ca>
2024-05-26 16:11:00 -0700

    finished function headers and getters/setters for item

--- src/model/Item.java
+++ src/model/Item.java
@@ -4,4 +4,55 @@ package model;
  * Represents an item needed for a project (apparel, gene scrolls, etc.)
  */
 public class Item {
+    private String name;
+    private ItemType type; // default = ItemType.OTHER
+    private int cost;
+    private boolean status; // default = false (i.e. not yet obtained)
+    private String link;
+
+    /**
+     * default constructor for Item.
+     */
+    public Item() {}
+
+    /**
+     * parameterized constructor for Item.
+     */
+    public Item(String name, ItemType type, int cost, boolean status, String link) {}
+
+    // getters and setters
+    public String getName() {
+      return name;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+    public void setType(ItemType newType) {
+        type = newType;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+    public void setCost(int newCost) {
+        cost = newCost;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+    public void setStatus(boolean newStatus) {
+        status = newStatus;
+    }
+
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String newLink) {
+        link = newLink;
+    }
 }
